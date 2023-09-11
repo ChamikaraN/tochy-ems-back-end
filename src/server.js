f862d584820f321db396c13443e11c07f0f77601
Chamikara Nayanajith <18645565+ChamikaraN@users.noreply.github.com>
2023-09-11 18:23:17 +0530

    Changers added

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import { fileURLToPath } from "url";
 import connectDB from "./DatabaseConnect/connectDB.js";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
+
 import employeeRoutes from "./routes/employeeRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
 import domainRoutes from "./routes/domainRoutes.js";
@@ -14,12 +15,7 @@ import domainRoutes from "./routes/domainRoutes.js";
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 import cors from "cors";
 import multer from "multer";
-import cron from "node-cron";
-// import { getAllBusinessList } from './controllers/userController.js'
-import { emailFunc } from "./utils/email/email.js";
-import { deleteEmail } from "./controllers/productController.js";
 import setupSendRandomEmailCron from "./cron-jobs/send-random-email-cron.js";
-import sendEmailUtil from "./utils/sendEmail.js";
 import sendMonthlyStatusCron from "./cron-jobs/send-monthly-status-cron.js";
 
 const storage = multer.diskStorage({});
@@ -36,27 +32,23 @@ app.use(express.urlencoded({ extended: true }));
 //   credentials: true, //access-control-allow-credentials:true
 //   optionSuccessStatus: 200,
 // };
-app.use(cors());
-
-// my comment
-// var whitelist = [
-//   "http://localhost:3000",
-//   "http://localhost:3000/dashboard",
-//   "http://127.0.0.1:3000",
-//   "https://app.phishstops.com",
-// ];
-// var corsOptionsDelegate = function (req, callback) {
-//   var corsOptions;
-//   if (whitelist.indexOf(req.header("Origin")) !== -1) {
-//     corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
-//   } else {
-//     corsOptions = { origin: false }; // disable CORS for this request
-//   }
-//   callback(null, corsOptions); // callback expects two parameters: error and options
-// };
-// app.use(cors(corsOptionsDelegate));
-// my comment
-
+// app.use(cors());
+var whitelist = [
+  "http://localhost:3000",
+  "http://localhost:3000/dashboard",
+  "http://127.0.0.1:3000",
+  "https://app.phishstops.com",
+];
+var corsOptionsDelegate = function (req, callback) {
+  var corsOptions;
+  if (whitelist.indexOf(req.header("Origin")) !== -1) {
+    corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false }; // disable CORS for this request
+  }
+  callback(null, corsOptions); // callback expects two parameters: error and options
+};
+app.use(cors(corsOptionsDelegate));
 // app.use((req, res, next) => {
 //   res.header('Access-Control-Allow-Origin', '*');
 //   next();
@@ -73,33 +65,25 @@ app.get("/emailseen", (req, res) => {
 app.use("/api/template", upload.single("image"), productRoutes);
 app.use("/api/user", userRoutes);
 // app.use('/api/business', businessRoutes)
-app.use("/api/employee", upload.single("csv"), employeeRoutes);
+app.use("/api/employee", employeeRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/domain", domainRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
-
-const PORT = process.env.PORT || 3001;
-
-// my comment
-// const __filename = fileURLToPath(import.meta.url);
-
-// const __dirname = path.dirname(__filename);
-
-// const options = {
-//   key: fs.readFileSync(path.join(__dirname, "./certificates/key.pem")),
-//   cert: fs.readFileSync(path.join(__dirname, "./certificates/cert.pem")),
-// };
-// const sslServer = https.createServer(options, app);
-// sslServer.listen(PORT, () => {
-//   console.log(`Secure server is listening oN PORT - ${PORT}`);
-// });
-// my comment
-
 setupSendRandomEmailCron();
 sendMonthlyStatusCron();
+const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+
+const options = {
+  key: fs.readFileSync(path.join(__dirname, "./certificates/key.pem")),
+  cert: fs.readFileSync(path.join(__dirname, "./certificates/cert.pem")),
+};
+const sslServer = https.createServer(options, app);
+sslServer.listen(PORT, () => {
+  console.log(`Secure server is listening oN PORT - ${PORT}`);
 });
---
+++ src/server.new.js
@@ -0,0 +1,105 @@
+import express from "express";
+import dotenv from "dotenv";
+import https from "https";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+import connectDB from "./DatabaseConnect/connectDB.js";
+import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import employeeRoutes from "./routes/employeeRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
+import domainRoutes from "./routes/domainRoutes.js";
+
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
+import cors from "cors";
+import multer from "multer";
+import cron from "node-cron";
+// import { getAllBusinessList } from './controllers/userController.js'
+import { emailFunc } from "./utils/email/email.js";
+import { deleteEmail } from "./controllers/productController.js";
+import setupSendRandomEmailCron from "./cron-jobs/send-random-email-cron.js";
+import sendEmailUtil from "./utils/sendEmail.js";
+import sendMonthlyStatusCron from "./cron-jobs/send-monthly-status-cron.js";
+
+const storage = multer.diskStorage({});
+
+let upload = multer({ storage });
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// const corsOptions = {
+//   origin: "*",
+//   credentials: true, //access-control-allow-credentials:true
+//   optionSuccessStatus: 200,
+// };
+app.use(cors());
+
+// my comment
+// var whitelist = [
+//   "http://localhost:3000",
+//   "http://localhost:3000/dashboard",
+//   "http://127.0.0.1:3000",
+//   "https://app.phishstops.com",
+// ];
+// var corsOptionsDelegate = function (req, callback) {
+//   var corsOptions;
+//   if (whitelist.indexOf(req.header("Origin")) !== -1) {
+//     corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
+//   } else {
+//     corsOptions = { origin: false }; // disable CORS for this request
+//   }
+//   callback(null, corsOptions); // callback expects two parameters: error and options
+// };
+// app.use(cors(corsOptionsDelegate));
+// my comment
+
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   next();
+// });
+connectDB();
+
+app.get("/", (req, res) => {
+  res.json("API server running");
+});
+app.get("/emailseen", (req, res) => {
+  res.send("<h1>Email seen successfully!!</h1>");
+});
+
+app.use("/api/template", upload.single("image"), productRoutes);
+app.use("/api/user", userRoutes);
+// app.use('/api/business', businessRoutes)
+app.use("/api/employee", upload.single("csv"), employeeRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/domain", domainRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 3001;
+
+// my comment
+// const __filename = fileURLToPath(import.meta.url);
+
+// const __dirname = path.dirname(__filename);
+
+// const options = {
+//   key: fs.readFileSync(path.join(__dirname, "./certificates/key.pem")),
+//   cert: fs.readFileSync(path.join(__dirname, "./certificates/cert.pem")),
+// };
+// const sslServer = https.createServer(options, app);
+// sslServer.listen(PORT, () => {
+//   console.log(`Secure server is listening oN PORT - ${PORT}`);
+// });
+// my comment
+
+setupSendRandomEmailCron();
+sendMonthlyStatusCron();
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
